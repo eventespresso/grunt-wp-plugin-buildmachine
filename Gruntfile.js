70cd58b90f3a7b74924122979709ebf192103be4
Darren Ethier <darren@roughsmootheng.in>
2015-02-19 14:15:05 -0500

    fix template tag

--- Gruntfile.js
+++ Gruntfile.js
@@ -629,7 +629,7 @@ module.exports = function(grunt) {
 			notify: "Building POT files.",
 			options: {
 				text_domain: '<%= eeParams.text_domain %>',
-				dest: '<%= eeParams.archiveBasePath =>',
+				dest: '<%= eeParams.archiveBasePath %>',
 				encoding: 'UTF-8',
 				keywords: ['__', '_e', '__ngettext:1,2', '_n:1,2', '__ngettext_nooop:1,2', '_n_noop:1,2', '_c', '_nc:4c,1,2', '_x:1,2c', '_nx:4c,1,2', '_nx_noop:4c,1,2', '_ex:1,2c', 'esc_attr__', 'esc_attr_e', 'esc_attr_x:1,2c', 'esc_html__', 'esc_html_e', 'esc_html_x:1,2c'],
 				package_name: '<%= eeParams.name %>',
