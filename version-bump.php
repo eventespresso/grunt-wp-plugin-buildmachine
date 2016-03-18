26cc14d987852fb2201ba8cb0af01f2e56e92f61
Darren Ethier <darren@roughsmootheng.in>
2016-03-18 15:50:16 -0400

    make sure to only update version in version_file if variables not empty.

--- version-bump.php
+++ version-bump.php
@@ -49,6 +49,11 @@ switch( $type ) {
 		//we're not bumping, just replacing whatever string is in the version string with 'decaf', and dropping the last version numbers.  Since decaf is built on top of checked out tags, we'll just replace the second from last array index.
 		$version_split[3] = 'decaf';
 
+		//IF there is a micro version, then we need to make sure we remove.
+		if ( isset( $version_split[4] ) ) {
+			unset( $version_split[4] );
+		}
+
 		//we're also changing the plugin name and uri
 		preg_match( '/^[ \t\/*#@]*Plugin URI:(.*)$/mi', $version_file, $uri_matches );
 		preg_match( '/^[ \t\/*#@]*Plugin Name:(.*)$/mi', $version_file, $name_matches );
@@ -125,7 +130,9 @@ $new_version = implode( '.', $version_split );
 
 
 //replace versions in file with the new version_number.
-$version_file = preg_replace( "/$orig_version/", $new_version, $version_file );
+if ( ! empty( $orig_version ) && ! empty( $new_version ) ) {
+	$version_file = preg_replace( "/$orig_version/", $new_version, $version_file );
+}
 
 //if version type is decaf then let's update extra values in $version_file and the readme.txt as well.
 if ( $type == 'decaf' ) {
@@ -140,7 +147,7 @@ if ( $type == 'decaf' ) {
 
 	//get version file contents.
 	$readmeTxt = file_get_contents( $readmeFile );
-	$readmeTxt = preg_replace( "/^[\t\/*#@]*Stable tag:(.*)$/mi", 'Stable tag: ' . $new_version, $readmeTxt );
+	$readmeTxt = preg_replace( "/^[\t\/*#@]*Stable tag:.*$/mi", 'Stable tag: ' . $new_version, $readmeTxt );
 	file_put_contents( $readmeFile, $readmeTxt );
 }
 
