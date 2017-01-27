51dd8dfbb51fd160a93e048ecf88bf011767ebd2
Darren Ethier <darren@roughsmootheng.in>
2017-01-27 14:27:29 -0500

    remove debugging

--- Gruntfile.js
+++ Gruntfile.js
@@ -1282,7 +1282,6 @@ module.exports = function(grunt) {
 
 	grunt.registerTask( 'maybeRunNpm', 'Used to determine whether to run the npm run buld task. Currently only runs if the jsBuildDirectory is set in the config.', function maybeRunNpm() {
 		var params = grunt.config.get('eeParams');
-        grunt.verbose.writeln( console.log(params) );
 		if (params.jsBuildDirectory !== '') {
             grunt.task.run('shell:npm_run');
 		}
