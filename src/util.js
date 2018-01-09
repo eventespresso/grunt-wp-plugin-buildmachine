3540cf4dcc901351effd61870a115b0c807a94f4
Darren Ethier <darren@roughsmootheng.in>
2018-01-09 13:46:47 -0500

    fix for remove_files task
    The info.json files will have paths in the old location (src/file_or_folder_to_remove) vs the new location (buildsrc/plugin_slug/file_or_folder_to_remove).  The fix that allows keeping info.json as is, is to assume that info.json either has the path defined relative to the plugin install in the buildmachine OR has `src/` prefixed and that will get replaced with the actual location.

--- src/util.js
+++ src/util.js
@@ -7,11 +7,16 @@ module.exports = {
         grunt = gruntObject;
     },
     rmPrepareFolders: function (folders_to_remove) {
-        var folders = [];
+        var folders = [],
+            pluginSrc = 'buildsrc/' + grunt.config.get('currentSlug') + '/';
         if (typeof folders_to_remove === 'undefined') {
             return folders;
         }
         for (var i = 0; i < folders_to_remove.length; i++) {
+            //replace `src/` with empty string
+            folders_to_remove[i] = folders_to_remove[i].replace('src/', '');
+            //prepend pluginSrc onto the path.
+            folders_to_remove[i] = pluginSrc + folders_to_remove[i];
             folders[i] = 'rm -rf ' + folders_to_remove[i];
         }
         return folders;
