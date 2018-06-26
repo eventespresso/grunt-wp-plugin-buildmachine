2232fd13ef08f0fe481f1df353c99ae8fcabf763
Darren Ethier <darren@roughsmootheng.in>
2018-06-26 11:12:18 -0400

    only update changelog if the file exists

--- src/transform.js
+++ src/transform.js
@@ -21,10 +21,23 @@ module.exports = {
     vidVersionReplace: function() {
         grunt.task.run(
             'shell:vid_version_replace',
-            'setNotifications:shell:vid_version_replace',
+            'setNotifications:shell:vid_version_replace'
+        );
+        this.vidVersionReplaceChangelog();
+        grunt.task.run(
             'gitadd:version',
             'gitcommit:vid_version_replace',
             'setNotifications:gitcommit:vid_version_replace'
         );
+    },
+    vidVersionReplaceChangelog: function() {
+        var params = grunt.config.get('pluginParams'),
+            packagePath = 'buildsrc/' + params.slug + '/CHANGELOG.md';
+        if ( grunt.file.exists(packagePath) ) {
+            grunt.task.run(
+                'shell:vid_version_replace_changelog',
+                'setNotifications:shell:vid_version_replace_changelog'
+            );
+        }
     }
 };
